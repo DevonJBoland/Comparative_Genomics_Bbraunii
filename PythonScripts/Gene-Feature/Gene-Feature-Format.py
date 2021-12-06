33616f435ad87b03ee5e60476c26fd43d276f567
Devon J. Boland <devonjboland@tamu.edu>
2021-12-06 17:01:51 -0600

    Update to Gene-Feature.py script adding filtering of gtf files, still needs to be tested on large alga gtf files. Could likely use a multithreading block to decrease run time.

--- PythonScripts/Gene-Feature/Gene-Feature-Format.py
+++ PythonScripts/Gene-Feature/Gene-Feature-Format.py
@@ -1,12 +1,35 @@
 #!/Users/devonboland/anaconda3/envs/Python-Bubbles/bin/python3
 
 import pandas as pd
+import re
 
 # Function to format GTF data by feature
 # Need to add a filtering step where genes are removed if they do not contain all features
 def extract(x):
     col_names = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute']
     data = pd.read_csv(x, index_col=False, header=None, delimiter="\t", names=col_names)
+    # Prefiltering Step
+    incomplete_genes_df = pd.DataFrame(columns=col_names)
+    complete_genes_df = pd.DataFrame(columns=col_names)
+    transcript_ID = [data.at[i, 'attribute'] for i in data.index if data.at[i, 'feature'] == 'transcript']
+    for i in transcript_ID:
+        temp_df = pd.DataFrame(columns=col_names)
+        for x in range(0, len(data.index)):
+            if re.search(i, data.at[x, 'attribute']):
+                entry = data.loc[x]
+                temp_df = temp_df.append(entry, ignore_index=True)
+        feature_checklist = [temp_df.at[i, 'feature'] for i in temp_df.index]
+        if feature_checklist.count('5\'-UTR') >= 1 and feature_checklist.count('start_codon') == 1 and \
+            feature_checklist.count('CDS') >= 1 and feature_checklist.count('stop_codon') == 1 and \
+            feature_checklist.count('3\'-UTR') >= 1:
+            complete_genes_df = complete_genes_df.append(temp_df, ignore_index=True)
+        else:
+            incomplete_genes_df = incomplete_genes_df.append(temp_df, ignore_index=True)
+    complete_genes_df.to_csv("Arace-complete-filtered.gtf", sep="\t", header=True, index=False)
+    incomplete_genes_df.to_csv("Arace-incomplete-filtered.gtf", sep="\t", header=True, index=False)
+
+    # Extraction Step
+    """
     gene = pd.DataFrame(columns=col_names)
     five_UTR = pd.DataFrame(columns=col_names)
     start = pd.DataFrame(columns=col_names)
@@ -18,7 +41,7 @@ def extract(x):
         if data.at[i, 'feature'] == 'gene':
             entry = data.loc[i]
             gene = gene.append(entry, ignore_index=True)
-        """elif data.at[i, 2] == '5\'-UTR':
+        elif data.at[i, 2] == '5\'-UTR':
 
         elif data.at[i, 2] == 'start_codon':
 
@@ -27,6 +50,7 @@ def extract(x):
         elif data.at[i, 2] == 'stop_codon':
 
         elif data.at[i, 2] == '3\'-UTR':
+
 """
 
 
