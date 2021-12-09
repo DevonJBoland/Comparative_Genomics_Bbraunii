da7ba3d8932aaa3ca20d2de85a650369ecb629f2
Devon J. Boland <devonjboland@tamu.edu>
2021-12-08 19:38:29 -0600

    Fresh update

--- PythonScripts/Gene-Feature/Gene-Feature-Format.py
+++ PythonScripts/Gene-Feature/Gene-Feature-Format.py
@@ -2,34 +2,43 @@
 
 import pandas as pd
 import re
+import concurrent.futures as cf
+
+
 
 # Function to format GTF data by feature
 # Need to add a filtering step where genes are removed if they do not contain all features
-def extract(x):
+def screen(x):
     col_names = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute']
     data = pd.read_csv(x, index_col=False, header=None, delimiter="\t", names=col_names)
     # Prefiltering Step
     incomplete_genes_df = pd.DataFrame(columns=col_names)
     complete_genes_df = pd.DataFrame(columns=col_names)
     transcript_ID = [data.at[i, 'attribute'] for i in data.index if data.at[i, 'feature'] == 'transcript']
-    for i in transcript_ID:
+
+    def threaded_screen(transcript):
         temp_df = pd.DataFrame(columns=col_names)
-        for x in range(0, len(data.index)):
-            if re.search(i, data.at[x, 'attribute']):
-                entry = data.loc[x]
+        for y in range(0, len(data.index)):
+            if re.search(transcript, data.at[y, 'attribute']):
+                entry = data.loc[y]
                 temp_df = temp_df.append(entry, ignore_index=True)
         feature_checklist = [temp_df.at[i, 'feature'] for i in temp_df.index]
         if feature_checklist.count('5\'-UTR') >= 1 and feature_checklist.count('start_codon') == 1 and \
-            feature_checklist.count('CDS') >= 1 and feature_checklist.count('stop_codon') == 1 and \
-            feature_checklist.count('3\'-UTR') >= 1:
+                feature_checklist.count('CDS') >= 1 and feature_checklist.count('stop_codon') == 1 and \
+                feature_checklist.count('3\'-UTR') >= 1:
             complete_genes_df = complete_genes_df.append(temp_df, ignore_index=True)
         else:
             incomplete_genes_df = incomplete_genes_df.append(temp_df, ignore_index=True)
+
+
+    with cf.ProcessPoolExecutor() as executor:
+        executor.map(threaded_screen, transcript_ID)
+
     complete_genes_df.to_csv("Arace-complete-filtered.gtf", sep="\t", header=True, index=False)
     incomplete_genes_df.to_csv("Arace-incomplete-filtered.gtf", sep="\t", header=True, index=False)
 
-    # Extraction Step
-    """
+ """   # Extraction Step
+    
     gene = pd.DataFrame(columns=col_names)
     five_UTR = pd.DataFrame(columns=col_names)
     start = pd.DataFrame(columns=col_names)
@@ -50,9 +59,9 @@ def extract(x):
         elif data.at[i, 2] == 'stop_codon':
 
         elif data.at[i, 2] == '3\'-UTR':
-
 """
 
 
+
 with open("UTR-GTFs/Arace-augustus.hints_utr.gtf", "r") as file:
-    extract(file)
+    screen(file)
