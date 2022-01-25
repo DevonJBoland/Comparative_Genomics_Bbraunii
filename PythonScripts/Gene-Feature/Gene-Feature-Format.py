7ac5713b6c3e73cbd8ebc0c72b7cdba986a913b7
DevonBoland <devonjboland@tamu.edu>
2022-01-25 09:50:56 -0600

    Changes to the intron annotation script.

--- PythonScripts/Gene-Feature/Gene-Feature-Format.py
+++ PythonScripts/Gene-Feature/Gene-Feature-Format.py
@@ -2,13 +2,14 @@
 
 import pandas as pd
 import re
-import concurrent.futures as cf
+from toolz import interleave
+#import concurrent.futures as cf
 
 # Function to format GTF data by feature
 # Need to add a filtering step where genes are removed if they do not contain all features
 
 
-def format_data(x):
+def filter_genes(x):
     col_names = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute']
     data = pd.read_csv(x, index_col=False, header=None, delimiter="\t", names=col_names)
     # Prefiltering Step
@@ -29,16 +30,75 @@ def format_data(x):
         else:
             incomplete_genes_df = incomplete_genes_df.append(temp_df, ignore_index=True)
 
-        if isnegeativegene(temp_df):
-            temp_df = reversechunck(temp_df)
-            print(temp_df)
+        to_remove = temp_df.at[1, 'attribute']
+        data = data[data.attribute != to_remove]
+        if data.at[0, 'feature'] == 'gene':
+            data = data.iloc[2:, :]
+        else:
+            data = data.iloc[1:, :]
+        data = data.reset_index(drop=True)
+
+    # complete_genes_df.to_csv("test-complete-filtered.gtf", sep="\t", header=True, index=False)
+    # incomplete_genes_df.to_csv("test-incomplete-filtered.gtf", sep="\t", header=True, index=False)
+
+    return complete_genes_df
+
+
+def get_intron(gene_df):  # Not working properly to create a list of transcript IDs
+    col_names = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute']
+    transcript_ID = [gene_df.at[n, 'attribute'] for n in gene_df.index if gene_df.at[n, 'feature'] == 'transcript']
+    # if not transcript_ID:
+    #     print("transcript list is empty")
+    #     return
+    for transcript in transcript_ID:
+        temp_gene_df = pd.DataFrame(columns=col_names)
+        for y in range(0, len(gene_df.index)):
+            if re.search(transcript, gene_df.at[y, 'attribute']):
+                entry = gene_df.loc[y]
+                temp_gene_df = temp_gene_df.append(entry, ignore_index=True)
+        if isnegeativegene(temp_gene_df):
+            temp_df = reversechunck(temp_gene_df)
+        else:
+            temp_df = temp_gene_df
+        fiveprime_array = temp_gene_df[temp_gene_df.feature == '5\'-UTR']
+        cds_array = temp_gene_df[temp_gene_df.feature == 'CDS']
+        threeprime_array = temp_gene_df[temp_gene_df.feature == '3\'-UTR']
+        seqname = temp_df.at[1, 'seqname']
+        source = 'Custom_Intron_Script'
+        feature = 'intron'
+        score = '.'
+        strand = temp_df.at[1, 'strand']
+        frame = '.'
+        attribute = temp_df.at[1, 'strand']
+        temp_intron_df = pd.DataFrame(columns=col_names)
+        if len(fiveprime_array) > 1:
+            for n in fiveprime_array.index:
+                intron_start = int(fiveprime_array.at[n, 'end']+1)
+                intron_end = int(fiveprime_array.at[n+1, 'start']-1)
+                intron_annotations = [seqname, source, feature, intron_start, intron_end, score, strand, frame,
+                                      attribute]
+                entry = dict(zip(col_names, intron_annotations))
+                temp_intron_df = temp_intron_df.append(entry, ignore_index=True)
 
 
 
+
+
+        # if isnegeativegene(temp_df):
+        #     reverse_df = reversechunck(temp_df)
+        #     get_cds_intron(reverse_df)
+        # # test = temp_df
+        # if isnegeativegene(temp_df):
+        #     reverse_temp_df = reversechunck(temp_df)
+        #     if meetsgenecriteria(feature_checklist):
+        #         return 'Is Complete'
+        #     else:
+        #         return 'Not Complete'
+
+    
 
 def isnegeativegene(gene_chunck):
-    return gene_chunck.at[0, 'strand'] == '-'
-
+    return gene_chunck.at [1, 'strand'] in ['-', '- '] # there is irradic spacing in the strand column throughout the gtf files.
 
 def reversechunck(gene_chunck):
     transcript_array = gene_chunck[gene_chunck.feature == 'transcript']
@@ -70,4 +130,12 @@ def meetsgenecriteria(featurelist):
 
 
 with open("UTR-GTFs/test.gtf", "r") as file:
-    format_data(file)
+    completegenes = filter_genes(file)
+    # Test if the filtering step was a success
+    if completegenes.empty:
+        print("Genes Filtered = Failure!")
+    else:
+        print("Genes Filtered = Success!"+"\n"+f"Output is saved to...")  # Need to insert a string for saved file
+
+    get_intron(completegenes)
+
