a9424b6768a50a8c1532c05c985fda4377d8d699
DevonBoland <devonjboland@tamu.edu>
2022-01-25 09:51:17 -0600

    General changes to AlphaFold2 scripts to reflect new versions of software.

---
+++ Unix-Shell-Scripts/base_read_count.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+reads="insert read file name here"
+
+zgrep . ${reads} |
+     awk 'NR%4==2{c++; l+=length($0)}
+          END{
+                print "Number of reads: "c; 
+                print "Number of bases in reads: "l
+              }'
